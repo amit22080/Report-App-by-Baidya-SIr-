89c5dce8bd02b0866509ed1bb190f17316be8ae3
Amit Verma <amit22080@gmail.com>
2018-09-27 12:27:07 +0530

    Initial commit

---
+++ Report/Assets/Scripts/MySQL.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System;
+
+public class MySQL : MonoBehaviour {
+
+	public string Database;
+	public string Server;
+	public string Username;
+	public string Password;
+
+	private MySqlCommand cmd;
+	private MySqlDataReader reader;
+	private string currDatabase="";
+	private bool isDatabaseConnected=false;
+
+	// Use this for initialization
+	void Start () {
+
+		connectDatabase();
+	}
+
+	#region Database Connection
+	void connectDatabase()
+	{
+		try
+		{
+			//datasource=localhost; database=amit; username=root; password=amit;
+			string conURL = "Datasource=" + Server + ";database=" +Database+";username=" + Username + ";password=" + Password + ";port=3306";
+			MySqlConnection con = new MySqlConnection(conURL);
+			con.Open();
+
+			cmd = con.CreateCommand();
+			cmd.CommandType = CommandType.Text;
+
+			//Creating database and again connecting
+			currDatabase = Database;
+
+			onConnection();
+
+			isDatabaseConnected = true;
+			print("CONNECTED to " + currDatabase);
+		}
+		catch (Exception e)
+		{
+			print(e.Message);
+		}
+	}
+
+	void onConnection()
+	{
+		//Creating a table if not exists
+		string query = "CREATE TABLE IF NOT EXISTS users(userID VARCHAR(50) UNIQUE PRIMARY KEY,password VARCHAR(50) NOT NULL,Gender CHAR default 'M',UserType VARCHAR(10) default'Student')";
+		executeUpdate(query);
+	}
+	#endregion
+
+	public void executeUpdate(string query)
+	{
+			cmd.CommandText=query;
+			cmd.ExecuteNonQuery();
+	}
+
+	public MySqlDataReader executeQuery(string query)
+	{
+		
+		return null;
+	}
+}
---
+++ Report/Assets/Scripts/Register.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Register : MonoBehaviour {
+
+	MySQL mysql;
+
+	public Text username,password,gender,userType;
+
+	void Start()
+	{
+		mysql = GetComponent<MySQL>();
+	}
+
+	public void registerUser()
+	{
+		try
+		{
+			if (!(string.IsNullOrEmpty(username.text) || string.IsNullOrWhiteSpace(username.text) || string.IsNullOrEmpty(password.text) || string.IsNullOrWhiteSpace(password.text)))
+			{
+				string query = "INSERT INTO users VALUES('" + username.text + "','" + password.text + "','" + gender.text.ToCharArray()[0] + "','" + userType.text + "');";
+				mysql.executeUpdate(query);
+			}
+			else
+			{
+				Debug.LogError("One of the fields is blank,Plz fill and try again");
+			}
+
+			print("REGISTRATION SUCCESSFUL!");
+		}
+		catch (Exception e)
+		{
+			Debug.LogError(e.Message.ToString());
+		}
+	}
+
+	
+	
+}
